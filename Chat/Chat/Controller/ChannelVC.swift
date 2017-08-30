b0ae9d64418747fbcb0e5309f1af44baeabb0ba7
admin <admin@MacBook-Pro-admin.local>
2017-08-30 17:14:07 +0300

    End of lesson 1.1

---
+++ Chat/Chat/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Chat
+//
+//  Created by admin on 30.08.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+    }
+
---
+++ Chat/Chat/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  Chat
+//
+//  Created by admin on 30.08.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+}
