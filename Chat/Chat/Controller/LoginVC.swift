f4661fb34b169714864f8ef26c49485167eb40a4
admin <admin@MacBook-Pro-admin.local>
2017-09-01 13:23:26 +0300

    lesson 4

---
+++ Chat/Chat/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Chat
+//
+//  Created by admin on 01.09.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Chat/Chat/New Group/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Chat
+//
+//  Created by admin on 01.09.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import Foundation
+
+//Segue
+let TO_LOGIN = "toLogin"
